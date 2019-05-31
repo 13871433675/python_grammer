e8362310bbe882dd238e38a5679c8b4c19e0ae09
qizhi.wang <qizhi.wang@rograndec.com>
2019-05-31 15:14:05 +0800

    commit to github

--- func/demo05.py
+++ func/demo05.py
@@ -1,1 +1,9 @@
 #命名关键字参数
+# 如果要限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数。这种方式定义的函数如下
+def  person(name,age,*,city,job):
+    print(name,age,city,job)
+
+
+
+if __name__ == '__main__':
+    person('wangqizhi',30,city='wuhan',job='coder')
