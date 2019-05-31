efcbc332dd433a41731a656dd4965a29143c51c7
qizhi.wang <qizhi.wang@rograndec.com>
2019-05-31 16:13:36 +0800

    commit to github

--- advancedFeatures/generator.py
+++ advancedFeatures/generator.py
@@ -1,1 +1,24 @@
 #生成器
+g =(x*x for x in range(1,11))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+
+
+iter = iter([1,3,6,7,87])
+
+print(next(iter))
+print(next(iter))
+print(next(iter))
+print(next(iter))
+print(next(iter))
+try:
+    next(iter)
+    print("success")
+except StopIteration:
+    print("over")
